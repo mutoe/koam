b5b63f818c339b257cbbce7ff960d0966149eb38
mutoe <imutoe@gmail.com>
2022-10-23 14:11:08 +0800

    chore: setup git hooks

--- src/koa.ts
+++ src/koa.ts
@@ -1,5 +1,13 @@
+import http from 'node:http'
+
 export default class Koa {
-  listen (): void {
-    // TODO: need to implement
+  private httpServer: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>
+
+  constructor () {
+    this.httpServer = http.createServer()
+  }
+
+  listen (...arguments_: Parameters<http.Server['listen']>): void {
+    this.httpServer.listen(...arguments_)
   }
 }
