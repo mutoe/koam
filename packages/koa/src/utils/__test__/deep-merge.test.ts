c256fd8d9a2af42e6aa7327d832ca8e148a83bb0
mutoe <imutoe@gmail.com>
2022-10-26 20:59:03 +0800

    feat: application config

---
+++ packages/koa/src/utils/__test__/deep-merge.test.ts
@@ -0,0 +1,57 @@
+import { deepMerge } from '../deep-merge'
+
+describe('# deepMerge', () => {
+  it('should not modify source object and target object', () => {
+    const source = { a: 'a' }
+    const target = { b: 'b' }
+    const result = deepMerge(source, target)
+
+    expect(source).toEqual({ a: 'a' })
+    expect(target).toEqual({ b: 'b' })
+    expect(result).toEqual({ a: 'a', b: 'b' })
+  })
+
+  it('should always target when source or target is not an object', () => {
+    expect(deepMerge(1, {})).toEqual({})
+    expect(deepMerge({}, 1)).toEqual({})
+    expect(deepMerge(null, {})).toEqual({})
+    expect(deepMerge({}, null)).toEqual({})
+  })
+
+  it('should can handle nested object', () => {
+    const source = {
+      a: 'a',
+      b: {
+        c: 'c',
+        d: {
+          e: 'e',
+        },
+      },
+      f: {},
+    }
+    const target = {
+      b: {
+        d: {
+          e: 'e1',
+          i: null,
+        },
+        g: 'g',
+      },
+      f: null,
+    }
+    const result = deepMerge(source, target)
+
+    expect(result).toEqual({
+      a: 'a',
+      b: {
+        c: 'c',
+        d: {
+          e: 'e1',
+          i: null,
+        },
+        g: 'g',
+      },
+      f: {},
+    })
+  })
+})
---
+++ packages/koa/src/utils/deep-merge.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/ban-types */
+
+type NotObject = undefined | null | number | string | boolean | Function | Symbol | any[]
+
+export function deepMerge <T extends object, U extends object> (source: T, target: U): T & U
+export function deepMerge <T extends NotObject, U> (source: T, target: U): U
+export function deepMerge <T, U extends NotObject> (source: T, target: U): T
+export function deepMerge <T, U> (source: T, target: U): (T & U) | U | T {
+  if (source === undefined) return target
+  if (target === undefined) return source
+  if (typeof source !== 'object' || source === null) return target
+  if (typeof target !== 'object' || target === null) return source
+  const result: any = {}
+  const keys = [...new Set([...Object.keys(source || {}), ...Object.keys(target || {})])]
+  for (const key of keys) {
+    const valueFormSource = source[key as keyof typeof source] as any
+    const valueFromTarget = target[key as keyof typeof target] as any
+    result[key] = deepMerge(valueFormSource, valueFromTarget)
+  }
+  return result
+}
