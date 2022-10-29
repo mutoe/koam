fe740859ff03b9b953fa9d16a38bcee656216cc0
mutoe <imutoe@gmail.com>
2022-10-30 00:46:00 +0800

    fix: parseQuery support simple array

--- packages/koa/src/utils/__test__/query-string.spec.ts
+++ packages/koa/src/utils/__test__/query-string.spec.ts
@@ -8,8 +8,8 @@ describe('# query string', () => {
       { qs: 'foo=1&bar=false', expected: { foo: 1, bar: false } },
       { qs: 'foo=-1&bar=true', expected: { foo: -1, bar: true } },
       { qs: 'foo=0&bar=', expected: { foo: 0, bar: '' } },
+      { qs: 'foo=0&foo=true&foo=bar', expected: { foo: [0, true, 'bar'] } },
       // TODO: query string parse array format
-      // { qs: 'foo=0&foo=true&foo=bar', expected: { foo: [0, true, 'bar'] } },
       // { qs: 'foo[]=0&foo[]=true&foo[]=bar', expected: { foo: [0, true, 'bar'] } },
       { qs: 'foo=0,1,bar', expected: { foo: '0,1,bar' } },
       { qs: 'foo', expected: { foo: '' } },
@@ -17,7 +17,7 @@ describe('# query string', () => {
       { qs: '=a', expected: {} },
     ]
 
-    it.each(testcases)('should return object correctly when passed "$qs"', ({ qs, expected }) => {
+    it.each(testcases)('when passed "$qs"', ({ qs, expected }) => {
       expect(parseQuery(qs)).toEqual(expected)
     })
   })
