597f5e172221bfe95c78076e054d82a47be8fbbe
mutoe <imutoe@gmail.com>
2022-10-30 00:46:00 +0800

    fix: parseQuery support simple array

--- src/utils/query-string.ts
+++ src/utils/query-string.ts
@@ -1,11 +1,22 @@
-export function parseQuery (queryString: string): Record<string, any> {
+/* eslint-disable max-statements-per-line */
+
+type QueryValue = string | boolean | number | undefined
+
+export function parseQuery (queryString: string): Record<string, QueryValue | QueryValue[]> {
   if (!queryString) return {}
   const entries = [...new URLSearchParams(queryString).entries()]
-  return Object.fromEntries(entries.map(([k, v]) => {
-    if (!k) return []
-    try {
-      v = JSON.parse(v)
-    } catch {}
-    return [k, v]
-  }))
+  const result: Record<string, QueryValue | QueryValue[]> = {}
+  for (let [k, v] of entries) {
+    if (!k) continue
+    try { v = JSON.parse(v) } catch {}
+    const prevValue = result[k]
+    if (prevValue === undefined) {
+      result[k] = v
+    } else if (Array.isArray(prevValue)) {
+      prevValue.push(v)
+    } else {
+      result[k] = [prevValue, v]
+    }
+  }
+  return result
 }
