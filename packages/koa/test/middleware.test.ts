f82e9c1fef018f2cb1771ada4d3d65bb552187cf
mutoe <imutoe@gmail.com>
2022-10-29 19:45:32 +0800

    test: split middleware and context tests

--- packages/koa/test/middleware.test.ts
+++ packages/koa/test/middleware.test.ts
@@ -9,23 +9,6 @@ describe('# middleware', () => {
   beforeEach(() => { testAddress = 33_000; app = new Koa() })
   afterEach(() => app.close())
 
-  it('should can get request basic information', async () => {
-    testAddress = app.use(cb).listen(0).address()
-
-    const path = '/path'
-    const query = '?foo=1&bar=true&baz=baz'
-    await fetch(`${baseUrl()}${path}${query}`, { method: 'GET' })
-
-    expect(cb).toHaveBeenCalledTimes(1)
-    const ctx = cb.mock.calls[0][0]
-    expect(ctx).toMatchObject({
-      method: 'GET',
-      url: path + query,
-      path,
-      query: { foo: 1, bar: true, baz: 'baz' },
-    })
-  })
-
   describe('onion model', () => {
     it('should executing middleware in the correct order', async () => {
       app.use(async (ctx, next) => { cb(1); await next(); cb(2) })
@@ -51,25 +34,6 @@ describe('# middleware', () => {
       await fetch(baseUrl())
 
       expect(cb).toHaveBeenCalledTimes(1)
-    })
-  })
-
-  describe('parse request body', () => {
-    it('should parse json response correctly', async () => {
-      testAddress = app.use(cb).listen(0).address()
-
-      await fetch(baseUrl(), {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({ foo: 'bar', count: 1 }),
-      })
-
-      expect(cb).toHaveBeenCalledTimes(1)
-      const ctx = cb.mock.calls[0][0]
-      expect(ctx.method).toBe('POST')
-      expect(ctx.body).toEqual({ foo: 'bar', count: 1 })
     })
   })
 })
