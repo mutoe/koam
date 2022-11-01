544e1b6c5ca2c55aeaf18cb7e7366153c2b6f806
mutoe <imutoe@gmail.com>
2022-11-02 01:37:20 +0800

    feat: implement response header setters and getter

--- packages/koa/src/response.ts
+++ packages/koa/src/response.ts
@@ -2,6 +2,7 @@ import http from 'node:http'
 import Application from 'src/application'
 import { Context } from 'src/context'
 import { HttpStatus } from 'src/enums/http-status'
+import { Koa } from 'src/index'
 
 export default class Response {
   readonly app: Application
@@ -18,4 +19,35 @@ export default class Response {
 
   get status (): HttpStatus { return this.#res.statusCode }
   set status (val: HttpStatus) { this.#res.statusCode = val }
+
+  get headerSent (): boolean { return this.#res.headersSent }
+  get headers (): http.OutgoingHttpHeaders { return this.#res.getHeaders() }
+  flushHeaders = (): void => this.#res.flushHeaders()
+  has = (key: Koa.HeaderKey): boolean => this.#res.hasHeader(key as string)
+  get = (key: Koa.HeaderKey): Koa.HeaderValue | undefined =>
+    this.#res.getHeader(key as string)
+
+  set = (key: Koa.HeaderKey, value: Koa.HeaderValue): this => {
+    if (this.headerSent) return this
+    this.#res.setHeader(String(key), value)
+    return this
+  }
+
+  append = (key: Koa.HeaderKey, val: Koa.HeaderValue): this => {
+    if (this.headerSent) return this
+    const prev = this.get(key)
+    if (prev) {
+      if (!Array.isArray(val)) val = [String(val)]
+      val = Array.isArray(prev)
+        ? [...prev, ...val]
+        : [String(prev), ...val]
+    }
+    return this.set(key, val)
+  }
+
+  remove (key: Koa.HeaderKey): this {
+    if (this.headerSent) return this
+    this.#res.removeHeader(String(key))
+    return this
+  }
 }
